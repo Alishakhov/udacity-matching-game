e9aed2957ccf154c51f6122c728a4bbb4e506dc8
AlishaKhov <alishakhov@gmail.com>
2019-05-15 23:54:17 -0700

    Display two matched cards

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-let deck = document.querySelector(".deck");
-let card = document.querySelectorAll(".card");
+let deck = document.querySelector(".deck"),
+    card = document.querySelectorAll(".deck .card"),
+    cardIcons = document.querySelectorAll(".deck .card i"),
+    restart = document.querySelector(".restart"),
+    restartIcon = document.querySelector(".restart i"),
+    displayCards = [];
+   
+//let shuffleIcons = Array.from(cardIcons);
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -17,13 +22,22 @@ function shuffle(array) {
 
 function initGame() {
     deck.addEventListener("click", function(e) {
-        // e.target.style.background = "yellow";
         e.target.classList.add("open", "show");
-         console.log("the card is clicked");
+        displayCards.push(e.target);
+        if(displayCards.length === 2) {
+            if(displayCards[0].firstElementChild.className === displayCards[1].firstElementChild.className) {
+                displayCards[0].classList.add("open", "show", "match");
+                displayCards[1].classList.add("open", "show", "match");
+                displayCards = [];
+            }else{
+                console.log("not match");
+                displayCards = [];
+            }
+        }
+        
      });
 }
 
 initGame();
 console.log(card);
-
 
